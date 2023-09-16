fb552d62fdab92e2a4b7131828ad76cc2933f453
ElisonHenrique <elisondanielhenrique@gmail.com>
2023-09-16 15:41:16 -0300

    criação de dados da ficha de inscrição

---
+++ BackEnd/ProtoAcademyAPI/ProtoAcademyAPI/Entities/Aluno.cs
@@ -0,0 +1,22 @@
+﻿namespace ProtoAcademyAPI.Entities
+{
+    public class Aluno
+    {
+        public int matricula {  get; set; }
+
+        public string Nome { get; set; }
+
+        public string Idade { get; set; }
+
+        public string Data_de_Nascimento { get; set;}
+
+        public string? Sexo { get; set;}
+
+        public string CPF { get; set;}
+
+        public string?  Endereço { get; set;}
+
+
+           
+    }
+}
